e1a65e6953048c1d226d44330c3745effd896e9a
najeeb hall <naj404@hotmail.com>
2017-12-04 01:16:46 +0000

    final touches

---
+++ Views/Login.view.php
@@ -0,0 +1,12 @@
+<div id="Login-Div" >
+    <span  id ='LoginTitle'>Welcome To CheapoMail</span><br>
+    <div id="Login-Controls">          
+        <input id ='username' value="QuewayneSeaton"class = "loginItems" name="UserName" type="text" placeholder="Enter your User Name"> <br>
+        <input id ='password'value='Pineapple123'class = "loginItems" name="Password" type="text" placeholder="Enter your Password"><br>
+        <p  id ='validation-Error'>Please Login</p><br>
+    </div>
+</div>
+
+
+
+
---
+++ Views/home.view.php
@@ -0,0 +1,18 @@
+<div id="Background-Div"> 
+            <div id="Contact-title"><span>Contacts</span> </div>
+            <div class="column" id="Contacts-Div"> 
+                   
+            </div>
+            
+            <div class="column" id="Messaging-Section" > 
+                
+                <div id="Current-Chat">Click on a Contact to load their chat </div>
+                <div id="Messages-Div"> </div>
+                <div id="Controls-Div"> 
+                
+                    <input id ='Text-Box'name="Text-Box" type="text" placeholder="Enter your Message here">
+                    <input id ='Send-Message' class="Hover"  type="Submit" name="send-Message" value="Send"/>
+                </div>
+            </div>
+</div>
+       
